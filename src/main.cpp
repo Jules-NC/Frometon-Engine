ab51a65b0652a1e3975eb4cb560a3c9667e6d908
Jules Neghnagh--Chenavas <jules.nc@protonmail.com>
2019-07-09 23:56:59 +0200

    Issue #23 resolved. GLFW is now called, initialised and "tout le bouzin"  from a singleton.

--- src/main.cpp
+++ src/main.cpp
@@ -75,20 +75,18 @@ GLint common_get_shader_program(const char *vertex_shader_source, const char *fr
 int main(void) {
     GLuint shader_program, vbo;
     GLint pos;
-    GLFWwindow* window;
+    SControls::getInstance().init(WIDTH ,HEIGHT);
+    GLFWwindow* window = SControls::getInstance().getWindows();
 
 // [Link tests]
-	int lol = 500;   
+    int lol = 500;
      //SControls::getInstance().init(lol, lol);
-	Assimp::Importer importer;
+    Assimp::Importer importer;
 // [/link tests]
-
-    glfwInit();
-    window = glfwCreateWindow(WIDTH, HEIGHT, __FILE__, NULL, NULL);
     glfwMakeContextCurrent(window);
 
-    printf("GL_VERSION  : %s\n", glGetString(GL_VERSION) );
-    printf("GL_RENDERER : %s\n", glGetString(GL_RENDERER) );
+    printf("GL_VERSION  : %s\n", glGetString(GL_VERSION));
+    printf("GL_RENDERER : %s\n", glGetString(GL_RENDERER));
     
     shader_program = common_get_shader_program(vertex_shader_source, fragment_shader_source);
     pos = glGetAttribLocation(shader_program, "position");
