6280ebd51444a2a6ff1af1c9383a89d8d6e7294a
Jules Neghnagh--Chenavas <jules.nc@protonmail.com>
2019-07-10 16:46:47 +0200

    Copied files from our rpevious engine in the repo. Not working for teh moment (weird error)

--- src/main.cpp
+++ src/main.cpp
@@ -1,114 +1,101 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define GLFW_INCLUDE_ES2
-#include <GLFW/glfw3.h>
-
+// STANDARDS
+#include <stdio.h>						// |
+#include <stdlib.h>						// |
+#include <vector>						// |
+#include <iostream>
+                                        // LIBS
+#include <GL/glew.h>					// |glew
+#include <GLFW/glfw3.h>						// |GLFW
+#include <glm/glm.hpp>					// |glm
+#include "shader.h"			// |
+#include "controls.h"			// |
+                                        // LIBS
+#include "CShape.h"
 #include "SControls.h"
+using namespace glm;				// NAMESPACE
+unsigned int WIDTH = 1024;
+unsigned int HEIGHT = 768;
+GLFWwindow * WINDOW;
+float SPEED;
+
+bool initGLEW();
+
+glm::mat4 CarCamera = glm::mat4();
 
 
-#include <assimp/Importer.hpp>                  // |Assimp
-
-
-static const GLuint WIDTH = 800;
-static const GLuint HEIGHT = 600;
-static const GLchar* vertex_shader_source =
-    "#version 100\n"
-    "attribute vec3 position;\n"
-    "void main() {\n"
-    "   gl_Position = vec4(position, 1.0);\n"
-    "}\n";
-static const GLchar* fragment_shader_source =
-    "#version 100\n"
-    "void main() {\n"
-    "   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
-    "}\n";
-static const GLfloat vertices[] = {
-        0.0f,  0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f,
-};
-
-GLint common_get_shader_program(const char *vertex_shader_source, const char *fragment_shader_source) {
-    enum Consts {INFOLOG_LEN = 512};
-    GLchar infoLog[INFOLOG_LEN];
-    GLint fragment_shader;
-    GLint shader_program;
-    GLint success;
-    GLint vertex_shader;
-
-    /* Vertex shader */
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
-    glCompileShader(vertex_shader);
-    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertex_shader, INFOLOG_LEN, NULL, infoLog);
-        printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s\n", infoLog);
+bool initGLEW() {
+    glewExperimental = true; // Needed for core profile
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        return false;
     }
 
-    /* Fragment shader */
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
-    glCompileShader(fragment_shader);
-    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragment_shader, INFOLOG_LEN, NULL, infoLog);
-        printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s\n", infoLog);
-    }
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    glDepthFunc(GL_LESS);
+    glEnable(GL_CULL_FACE);
 
-    /* Link shaders */
-    shader_program = glCreateProgram();
-    glAttachShader(shader_program, vertex_shader);
-    glAttachShader(shader_program, fragment_shader);
-    glLinkProgram(shader_program);
-    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shader_program, INFOLOG_LEN, NULL, infoLog);
-        printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s\n", infoLog);
-    }
-
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
-    return shader_program;
+    return true;
 }
 
-int main(void) {
-    GLuint shader_program, vbo;
-    GLint pos;
-    SControls::getInstance().init(WIDTH ,HEIGHT);
-    GLFWwindow* window = SControls::getInstance().getWindows();
 
-// [Link tests]
-    int lol = 500;
-     //SControls::getInstance().init(lol, lol);
-    Assimp::Importer importer;
-// [/link tests]
-    glfwMakeContextCurrent(window);
+int main(void)
+{
+    SControls::getInstance().init(WIDTH, HEIGHT);
+    WINDOW = SControls::getInstance().getWindows();
 
-    printf("GL_VERSION  : %s\n", glGetString(GL_VERSION));
-    printf("GL_RENDERER : %s\n", glGetString(GL_RENDERER));
-    
-    shader_program = common_get_shader_program(vertex_shader_source, fragment_shader_source);
-    pos = glGetAttribLocation(shader_program, "position");
+    initGLEW();
 
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glViewport(0, 0, WIDTH, HEIGHT);
+    // SHADERS
+    GLuint programID = LoadShaders("/lol.vs", "/lel.fs");
+    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
-    glEnableVertexAttribArray(pos);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    while (!glfwWindowShouldClose(window)) {
+    // ======================== DESSIN ============================
+    char * path = "../../parpaing.jpg";
+    char * objSquare = "../../square.obj";
+
+    CShape square = CShape();
+
+    square.init(objSquare, path);
+
+    // ======================== /DESSIN ===========================
+
+
+    double lastTime = glfwGetTime();
+    int nbFrames = 0;
+    do {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(programID);
+        double currentTime = glfwGetTime();
+
+        nbFrames++;
+        float deltaTime = currentTime - lastTime;
+
+        if (currentTime - lastTime >= 1.0) {
+            printf("%f ms/frame\n",1000- 1000.0 / double(deltaTime));
+            nbFrames = 0;
+            lastTime += 1.0;
+        }
+
+        computeMatricesFromInputs();
+        glm::mat4 ProjectionMatrix = getProjectionMatrix();
+        glm::mat4 ViewMatrix = getViewMatrix();
+        glm::mat4 MVP;
+
+        MVP = ProjectionMatrix * ViewMatrix;
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+        square.draw();
+
+        // Swap buffers
+        glfwSwapBuffers(WINDOW);
         glfwPollEvents();
-        glClear(GL_COLOR_BUFFER_BIT);
-        glUseProgram(shader_program);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glfwSwapBuffers(window);
     }
-    glDeleteBuffers(1, &vbo);
+    while (glfwGetKey(WINDOW, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+        glfwWindowShouldClose(WINDOW) == 0);
+
+    square.FreeMemory();
+    glDeleteProgram(programID);
     glfwTerminate();
-    return EXIT_SUCCESS;
+
+    return 0;
 }
