dd976b4142860d2e5da7cfebec2196710e2d714b
Jules Neghnagh--Chenavas <jules.nc@protonmail.com>
2019-07-16 23:52:57 +0200

    Update install_base.sh

--- install_base.sh
+++ install_base.sh
@@ -4,12 +4,5 @@ mkdir -p lib
 wget https://raw.githubusercontent.com/nothings/stb/master/stb_image.h -P src/ -nc
 
 wget https://anim1-s12.wp.rpi.edu/files/2012/03/TextureGrid.jpg -P res/ -nc
- 
 
-mkdir -p build
-cd build
-cmake ..
-make
-cd src
-./Frometon-Engine
 
