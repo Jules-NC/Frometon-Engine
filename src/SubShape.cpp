ffb05cbaa56210860bd7f98f06d1a3011c0411be
Jules Neghnagh--Chenavas <jules.nc@protonmail.com>
2019-07-25 21:20:42 +0200

    Added a crappy skybox ! The code is awfull We need to rectify this

--- src/SubShape.cpp
+++ src/SubShape.cpp
@@ -65,7 +65,7 @@ SubShape::SubShape(aiMesh * mesh, SubTexture * texture){
 
 void SubShape::draw(){
     this->texture->bindTexture();
-
+    glBindVertexArray(this->VAO);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
